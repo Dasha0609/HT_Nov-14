1fa2ba4b40c5dd117e9227618616c478a7351492
Dasha <dnazarkina@ya.ru>
2022-11-15 00:42:04 +0300

    added info for Ex2

---
+++ HT_Nov_Ex2/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+int a = 8;
+Console.WriteLine(a);
+
+int b = 4;
+Console.WriteLine(b);
+
+if (a > b)
+{
+    Console.Write(" the biggest number is ");
+    Console.WriteLine(a);
+}
+else
+{
+    Console.Write(" the biggets number is "); 
+    Console.WriteLine(b);
+}
+
+
+
