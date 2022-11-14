9f9160ada0f687c627bdae82892218435179dbcc
Dasha <dnazarkina@ya.ru>
2022-11-15 01:28:02 +0300

    added info to Ex4

--- HT_Nov_Ex4/Program.cs
+++ HT_Nov_Ex4/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+int a = 20;
+int b = 10;
+int c = 6;
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+
+if (a > b)
+{
+    if (a > c)
+    {
+        Console.Write("max is ");
+        Console.WriteLine(a);
+    }
+    else
+    {
+        Console.Write("max is ");
+        Console.WriteLine(c);
+    }
+}
+else
+if (b > c)
+{
+    Console.Write("max is ");
+    Console.WriteLine(b);
+}
+
