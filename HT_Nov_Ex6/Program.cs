ff5630b0b8698219241e7ca1c3399d6da28df875
Dasha <dnazarkina@ya.ru>
2022-11-15 11:50:24 +0300

    added info for Ex6

--- HT_Nov_Ex6/Program.cs
+++ HT_Nov_Ex6/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.Write("please enter your number:");
+string input = Console.ReadLine();
+int number = Convert.ToInt32(input);
+
+if (number % 2==0)
+{
+    Console.WriteLine("it's an even number");
+}
+else
+{
+    Console.WriteLine("it's an odd number");
+}
+
+
+
+
+
+
+
+
+
