965bc50a4f8d6f829982fa632297042011088268
Dasha <dnazarkina@ya.ru>
2022-11-15 17:58:42 +0300

    added info to Ex8

--- HT_Nov_Ex8/Program.cs
+++ HT_Nov_Ex8/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("please enter your number N:");
+string? input = Console.ReadLine();
+int N = Convert.ToInt32(input);
+int a = 1;
+
+for (int i = a; i <= N; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " , ");
+    }
+}
+
+Console.ReadKey();
+
+
