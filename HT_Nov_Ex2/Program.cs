3e8e93329c6fd449a4abb53be57e34d394210ddd
Dasha <dnazarkina@ya.ru>
2022-11-15 10:59:31 +0300

    changes incorporated

--- HT_Nov_Ex2/Program.cs
+++ HT_Nov_Ex2/Program.cs
@@ -1,19 +1,23 @@
 ﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-int a = 8;
+int a = 5;
 Console.WriteLine(a);
 
-int b = 4;
+int b = 15;
 Console.WriteLine(b);
 
 if (a > b)
 {
     Console.Write(" the biggest number is ");
     Console.WriteLine(a);
+    Console.Write(" the smallest number is ");
+    Console.WriteLine(b);
 }
 else
 {
     Console.Write(" the biggets number is "); 
     Console.WriteLine(b);
+    Console.Write(" the smallest number is ");
+    Console.WriteLine(a);
 }
 
 
