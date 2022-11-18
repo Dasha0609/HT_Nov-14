9f457adae9ef8e2f9f06d8d187e281bd38b975ae
Dasha <dnazarkina@ya.ru>
2022-11-18 16:22:55 +0300

    added some changes into Ex10

--- HT_Nov_Ex10/Program.cs
+++ HT_Nov_Ex10/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+
+Console.WriteLine("Please enter three-digit number");
+
+while (true)
+{
+    string input = Console.ReadLine();
+    Console.WriteLine("{0}->{1}",input, input[1]);
+    break;
+}
