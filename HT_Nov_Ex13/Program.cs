d72b0891b052e3d036f3f0a5432f622f3e816c25
Dasha <dnazarkina@ya.ru>
2022-11-18 19:13:10 +0300

    added changes into Ex13

--- HT_Nov_Ex13/Program.cs
+++ HT_Nov_Ex13/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+Console.WriteLine("Please enter three-digit number");
+string? input = Console.ReadLine();
+int a = Convert.ToInt32(input);
+int number = input[input.Length - 1] - '0';
+
+
+if (input.Length >= 3)
+{
+    Console.WriteLine("The third number is  " + input.ToString () + " = " + input.ToString()[2]);
+}
+else
+{
+    Console.WriteLine("There is no third number");
+}
+
+       
