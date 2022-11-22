1aae3179787ea5bb26a17a9d0f1d0b72eef6df9a
Dasha <dnazarkina@ya.ru>
2022-11-22 12:12:00 +0300

    added more info into Ex23

--- HT_Nov_Ex23/Program.cs
+++ HT_Nov_Ex23/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+
+int number = ReadInt("Pls enter your number: ");
+int i = 1;
+while(i <= number)
+{
+    Console.Write(Pow(i++) + " ");
+}
+
+int Pow(int a)
+{
+    return a * a * a;
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
