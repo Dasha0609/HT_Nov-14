2738df5451a7e6f790bbfde5fce0e8ab32b5fce6
Dasha <dnazarkina@ya.ru>
2022-11-22 12:53:57 +0300

    more info added into Ex 19

--- HT_Nov_Ex19/Program.cs
+++ HT_Nov_Ex19/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Write("Pls enter your five-digit number: ");
+string input = Console.ReadLine();
+int number = Convert.ToInt32(input);
+
+string value = number.ToString();
+string result = "";
+ for (int i = 0, j = value.Length - 1; (value.Length / 2 == 0 ? i < value.Length / 2 : i <= value.Length / 2); i++, j--)
+ {
+    if (value[i] == value[j])
+    {
+        result = "true"; Console.Write(number + " is palindrom");
+        continue;
+    }
+    result = "false"; Console.Write(number + " is not a palindron");
+    break;
+ }  
+ 
+
+
