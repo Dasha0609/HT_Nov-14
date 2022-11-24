41befcaa5c834a2c4e3844141217f4cf3b18ad03
Dasha <dnazarkina@ya.ru>
2022-11-24 19:05:25 +0300

    added info for Ex27

--- HT_Nov_Ex27/Program.cs
+++ HT_Nov_Ex27/Program.cs
@@ -1,2 +1,16 @@
 ﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
+Console.Write("Please enter your number: ");
+string input = Console.ReadLine();
+int number = Convert.ToInt32(input);
+
+int result = 0;
+
+while (number > 0)
+{
+    result = result + number % 10;
+    number = number/10;
+}
+
+Console.Write("colloquial of numbers is " + result);
+
