a174110348218abe68f2d1b7afedc0bd7d246bd7
Dasha <dnazarkina@ya.ru>
2022-11-24 19:24:25 +0300

    added info for Ex29

--- HT_Nov_Ex29/Program.cs
+++ HT_Nov_Ex29/Program.cs
@@ -1,2 +1,14 @@
 ﻿// Напишите программу, которая задаёт массив из N элементов и выводит их на экран. Вывод сделать отдельным методом.
 
+Console.WriteLine("Please enter arrey length: "); 
+string input = Console.ReadLine();
+int arrey = Convert.ToInt32(input);
+
+int [] number = new int [arrey];
+
+for (int i = 0; i < number.Length; i++)
+{
+    number[i] = new Random().Next(0,100);
+    Console.Write(number[i] + " ");
+}
+
