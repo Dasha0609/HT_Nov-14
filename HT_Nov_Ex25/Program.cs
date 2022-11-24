e43382cf50507d252f06a70d2efa358e6ebbb6fa
Dasha <dnazarkina@ya.ru>
2022-11-24 18:30:10 +0300

    added info into Ex25

--- HT_Nov_Ex25/Program.cs
+++ HT_Nov_Ex25/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Без использования функции Math.Pow.
+
+Console.Write("Please add first number: ");
+string inputA = Console.ReadLine();
+int numberA = Convert.ToInt32(inputA);
+
+Console.Write("Please add second number: ");
+string inputB = Console.ReadLine();
+int numberB = Convert.ToInt32(inputB);
+
+int result = 1;
+
+for (int i = 1; i <= numberB; i++)
+{
+    result =  result * numberA;
+}
+
+Console.Write(result);
