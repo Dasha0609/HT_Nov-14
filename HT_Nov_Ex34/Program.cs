4fb399776340220901af9aadc25c56e292f706a8
Dasha <dnazarkina@ya.ru>
2022-11-30 22:02:56 +0300

    added details in Ex34

--- HT_Nov_Ex34/Program.cs
+++ HT_Nov_Ex34/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.*/
+
+int size = ReadInt("Enter array length: ");
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+WriteArray(numbers);
+int count = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if ((numbers[i] % 2) == 0)
+        count++;
+}   
+    Console.Write(count);
+    Console.ReadKey();
+
+
+void FillArrayRandomNumbers(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        int randomNumber = new Random().Next(100, 1000);
+        array[i] = Convert.ToInt32(randomNumber);
+    }
+}
+
+void WriteArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+
