790e3ea58ce0d2bd886c72887eed1a701c0d613f
Dasha <dnazarkina@ya.ru>
2022-12-01 11:55:51 +0300

    added details to Ex 38

--- HT_Nov_Ex38/Program.cs
+++ HT_Nov_Ex38/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+int size = ReadInt("Enter array length: ");
+double[] numbers = new double[size];
+double max = 0;
+double min = 0;
+double diff = 0;
+
+FillArrayRandomNumbers(numbers);
+WriteArray(numbers);
+
+for (int i = 0; i < size; i++)
+{
+    if (numbers[i] < min)
+    {
+        min = numbers[i];
+    }
+    
+    if (numbers[i] > max)
+    {
+        max = numbers[i];
+    }
+diff = max - min;
+ }
+  Console.WriteLine($"the min is {min} and max is {max}");
+  Console.Write(diff);
+  Console.ReadKey();
+
+
+void FillArrayRandomNumbers(double[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        int randomNumber = new Random().Next(100, 1000);
+        array[i] = Convert.ToDouble(randomNumber) / 100;
+    }
+}
+
+void WriteArray(double[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
