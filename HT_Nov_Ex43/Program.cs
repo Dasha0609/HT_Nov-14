9e477d0a4a591dca1a6cbdbf382c50bc169baf61
Dasha <dnazarkina@ya.ru>
2022-12-02 00:56:57 +0300

    details added to Ex43

--- HT_Nov_Ex43/Program.cs
+++ HT_Nov_Ex43/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.*/
+
+Console.WriteLine("Pls enter point b1: ");
+int pointb1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Pls enter point k1: ");
+int pointk1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Pls enter point b2: ");
+int pointb2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Pls enter point k2: ");
+int pointk2 = Convert.ToInt32(Console.ReadLine());
+
+int x = -(pointb1 - pointb2) / (pointk1 - pointk2);
+int y = pointk1 * x + pointb1;
+
+Console.WriteLine($"lines will be crossed in point: {x} ; {y})");
